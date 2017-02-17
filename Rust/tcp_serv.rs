c4794ae4a66171c9e4857cda344d7fb65a5646f1
Shouren <kimi_ysr@yahoo.com>
2017-02-17 17:01:50 +0800

    Fix error in rust tcp

--- Rust/tcp_serv.rs
+++ Rust/tcp_serv.rs
@@ -2,9 +2,11 @@ use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 
 fn handle_client(mut stream: TcpStream) {
-    println!("Client Addr: {}", stream.peer_addr().unwrap_or_else(|e| panic));
+    println!("Client Addr: {}",
+             stream.peer_addr()
+                   .unwrap_or_else(|e| panic!("Get peer addr fails: {}", e)));
 
-    let mut buffer: [u8; 3] = [0; 3];
+    let mut buffer: [u8; 128] = [0; 128];
     stream.read(buffer.as_mut()).unwrap();
     println!("{}", String::from_utf8_lossy(buffer.as_ref()));
 
@@ -12,7 +14,7 @@ fn handle_client(mut stream: TcpStream) {
 }
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:8080")\
+    let listener = TcpListener::bind("127.0.0.1:8080")
                    .unwrap_or_else(|e| panic!("Failed to bind: {}", e));
 
     for stream in listener.incoming() {
