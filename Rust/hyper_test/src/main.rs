72281193a123b86d09a69a5d0be3909ee4bdbb53
Shouren <yangshouren@gmail.com>
2017-04-07 15:05:56 +0800

    Some code for hyper.rs

--- Rust/hyper_test/src/main.rs
+++ Rust/hyper_test/src/main.rs
@@ -11,12 +11,20 @@ extern crate pretty_env_logger;
 // use futures::stream::Stream;
 
 extern crate hyper;
+extern crate hyper_native_tls;
+
+use hyper::Client;
+use hyper::net::HttpsConnector;
+use hyper_native_tls::NativeTlsClient;
+use std::io::Read;
 
 use hyper::Client;
 
-
 fn main() {
     pretty_env_logger::init().unwrap();
+    let ssl = NativeTlsClient::new().unwrap();
+    let connector = HttpsConnector::new(ssl);
+    let client = Client::with_connector(connector);
     let url = "http://www.google.com/robots.txt";
     println!("Get from {0} ...", url);
     let client = Client::new();
