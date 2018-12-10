5bea5e56f3c694c810256d8ea4954fb9b1d919a9
Shouren Yang <yangshouren@gmail.com>
2018-12-10 18:19:19 +0800

    Rust: hello_world takes proxy_addr and target_proxy from cli parameters

--- Rust/hello_world/src/main.rs
+++ Rust/hello_world/src/main.rs
@@ -1,34 +1,43 @@
-extern crate reqwest;
-
-use std::env::var;
 use std::time::Instant;
+
 use reqwest::Url;
-
-
-
-fn get_proxy_addr() -> String {
-    let default_proxy_addr= "http://127.0.0.1:1087".to_string();
-    let key = "HTTP_PROXY";
-    match var(key) {
-        Ok(val) => return val,
-        Err(_)=> return default_proxy_addr,
-    }
-}
+use clap::{Arg, App};
 
 fn main() {
+    let params= App::new("Proxy time measure program")
+        .version("0.1.0")
+        .author("Shouren <yangshouren@gmail.com>")
+        .arg(Arg::with_name("proxy")
+            .short("p")
+            .long("proxy")
+            .takes_value(true)
+            .help("Proxy address for measurement")
+            .required(true))
+        .arg(Arg::with_name("target")
+            .short("t")
+            .long("target")
+            .takes_value(true)
+            .help("Target address to send request for measurement")
+            .default_value("https://www.google.com/generate_204"))
+        .get_matches();
+
+    let proxy_addr = params.value_of("proxy").expect("Missing proxy address");
+    let target_addr = params.value_of("target").expect("Missing target address");
     println!("Start proxy testing...");
+    println!("# Proxy: {}", proxy_addr);
+    println!("# Target: {}", target_addr);
+
     let proxy = reqwest::Proxy::all(
-        Url::parse(
-            get_proxy_addr().as_str()
-        ).expect("Invalid Proxy Config")
-    ).expect("Create Proxy Failed");
+        Url::parse(proxy_addr).expect("Invalid proxy address")
+    ).expect("Create proxy failed");
 
     let client = reqwest::Client::builder().proxy(proxy).build().expect("Create client failed");
 
+    println!("Getting proxy response...");
     let now = Instant::now();
-    let resp = client.get("https://www.google.com/generate_204").send().expect("Request Google 204 URL failed");
+    let resp = client.get(target_addr).send().expect(format!("Request URL({}) failed", target_addr).as_str());
     let latency = now.elapsed();
 
-    println!("Status Code: {:?}", resp.status());
-    println!("Time Duration: {:?}",  latency);
+    println!("# Status Code: {:?}", resp.status());
+    println!("# Time Duration: {:?}",  latency);
 }
